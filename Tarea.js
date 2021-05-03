5e4e981242517b7e2141654dcdc1f9b7347391c4
pepomontero10 <jmontero3@miuandes.cl>
2021-05-02 22:03:28 -0400

    Update Tarea.js

--- Tarea.js
+++ Tarea.js
@@ -1,1 +1,21 @@
-console.log("hola mundo")
+
+function fibonacci(numero){
+    if (numero === 1){
+      console.log("[0,1]")
+    }
+    else{
+      var x = 2
+      var nuevo = 1
+      var f = 0
+      var suma = 0
+      while(numero !== x-1){
+        suma = f + nuevo
+        f = nuevo
+        nuevo = suma
+        x += 1
+      }
+      console.log(suma)
+    }
+  }
+  
+fibonacci(8)
